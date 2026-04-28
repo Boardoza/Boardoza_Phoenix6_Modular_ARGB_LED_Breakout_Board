58c02660db05267c52f08ff57d2c7db0792af95c
Eray Eroğlu <103147175+eray-eroglu@users.noreply.github.com>
2026-04-28 11:28:51 +0300

    Fixed comment lines

--- software files/examples/main/main.ino
+++ software files/examples/main/main.ino
@@ -1,35 +1,35 @@
 #include <Arduino.h>
 #include "T3A33BRG.h"
 
-// LED Kontrol Pinleri
+// LED Control Pins
 #define DATA_PIN  25
 #define CLOCK_PIN 26
 
-// T3A33BRG Nesnesi
+// T3A33BRG Object
 T3A33BRG ledController(DATA_PIN, CLOCK_PIN);
 
 void setup() {
     Serial.begin(115200);
 
-    // LED kontrol modülünü başlat
+    // Initialize the LED control module
     ledController.begin();
 
-    // Başlangıçta tüm LED'leri sıfırla
+    // Reset all LEDs initially
     ledController.send32Bit(0, 0, 0, 0, 0);
-    Serial.println("12 adet 32-bit veri gönderildi.");
+    Serial.println("12 pieces of 32-bit data sent.");
 }
 
 void loop() {
-    // 12 LED'i sırayla yak
+    // Turn on 12 LEDs sequentially
     for (uint8_t i = 0; i < 12; i++) {
-        ledController.send32Bit(7, 0, 0, 0, 255);  // Mavi renkli LED
-        delay(1000); // 1 saniye bekle
+        ledController.send32Bit(7, 0, 0, 0, 255);  // Blue colored LED
+        delay(1000); // Wait 1 second
     }
     ledController.send32Bit(0, 0, 0, 0, 0);
-    // 12 LED'i sırayla söndür
+    // Turn off 12 LEDs sequentially
     for (uint8_t i = 0; i < 14; i++) {
-        ledController.send32Bit(7, 0, 0, 0, 0);  // LED kapalı
-        delay(1000); // 1 saniye bekle
+        ledController.send32Bit(7, 0, 0, 0, 0);  // LED off
+        delay(1000); // Wait 1 second
     }
     ledController.send32Bit(0, 0, 0, 0, 0);
 }
